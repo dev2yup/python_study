3c935f684404fc8ac35c7156f4a1cfe6e85ab2e9
leedy5521 <80202719+leedy5521@users.noreply.github.com>
2021-05-12 16:54:44 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ from random import *
 score = randint(0, 100)
 print(score)
 
-#score = int(input("점수 입력:"))
+#score = int(input("점수 입력:")
 if score>100 or score<0:
     print("""잘못된 점수입니다.
 프로그램을 종료.""")
