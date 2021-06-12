ce86222170028c5cd587fe3a7f83a6f629cb8257
leedy5521 <leedy5521@naver.com>
2021-06-12 16:48:13 +0900

    Study

--- study0/end_word_game.py
+++ study0/end_word_game.py
@@ -8,29 +8,35 @@ count2 = 3
 
 while True:
     input_str2 = input("%s님, '%s' 로 시작하는 단어를 입력하세요." %(user2, last_str))
-    if input_str2[0] == last_str:
-        last_str = input_str2[-1]
-        print(input_str2)
-    
-    elif input_str2[0] != last_str:
+    if input_str2[0] != last_str:
         count2 -= 1
         print("틀렸습니다. %s님의 기회는 %s번 남았습니다." %(user2, count2)) 
         if count2 == 0:
             print(user1, "님이 승리 하셨습니다.")
             break    
+    while input_str2[0] == last_str:
+        last_str = input_str2[-1]
+        print(input_str2)
+        if len(input_str2) < 2:
+            reinput_str2 = input("두 글자 이상의 단어를 입력하세요.")
+            input_str2 = reinput_str2
+            last_str = input_str2[-1]
+       
+    
     
     input_str1 = input("%s님, '%s' 로 시작하는 단어를 입력하세요." %(user1, last_str))
-    if input_str1[0] == last_str:
-        last_str = input_str1[-1]
-        print(input_str1)
-   
-     
-    elif input_str1[0] != last_str:
+    if input_str1[0] != last_str:
         count1 -= 1
         print("틀렸습니다. %s 님의 기회는 %s번 남았습니다." %(user1, count1))
         if count1 == 0:
             print(user2, "님이 승리하셨습니다.")
             break
-
+    while input_str1[0] == last_str:
+        last_str = input_str1[-1]
+        print(input_str1)
+        if len(input_str1) < 2:
+            reinput_str1 = input("두 글자 이상의 단어를 입력하세요.")
+            input_str1 = reinput_str1
+            last_str = input_str1[-1]
 
 
