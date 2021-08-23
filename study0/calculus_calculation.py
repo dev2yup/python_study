3015a112fcff1ca4bd67747b4ca9423077768bfd
leedy5521 <leedy5521@naver.com>
2021-08-23 12:21:57 +0900

    Study

--- study0/calculus_calculation.py
+++ study0/calculus_calculation.py
@@ -5,14 +5,14 @@ import numpy as np
 a, b = 0, 6 #이곳에 범위를 입력해주세요.
 m = max(a,b)
 n = min(a,b)
- 
 x = symbols('x') #기호변수 선언
  
-fx = input("함수")
-fx = sympify(fx)  # 이곳에 함수를 입력해주세요.
+fx = input("함수를 입력하세요. : ")
+fx = sympify(fx)
 fb = fx.subs(x, m) #함수에 m대입
 fa = fx.subs(x, n) #함수에 n대입
- 
+print(fb)
+print(fa)
 result = (fb - fa) / (b - a)
    
 print("평균변화율:", result,"입니다.")
